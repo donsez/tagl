3ad44eda724e740d6426604a32720d479330e96a
cpelliccia <cpelliccia@30627792-964a-4770-8836-e04b23abaf83>
2009-05-23 09:22:07 +0000

    License declaration fix
    git-svn-id: svn://svn.code.sf.net/p/cron4j/code/trunk@24 30627792-964a-4770-8836-e04b23abaf83

--- examples/1-quickstart/Main.java
+++ examples/1-quickstart/Main.java
@@ -1,20 +1,20 @@
 /*
  * cron4j - A pure Java cron-like scheduler
  * 
- * Copyright (C) 2008 Carlo Pelliccia (www.sauronsoftware.it)
+ * Copyright (C) 2007-2009 Carlo Pelliccia (www.sauronsoftware.it)
  * 
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU Lesser General Public License version
+ * 2.1, as published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License 2.1 for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License version 2.1 along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
  */
 import it.sauronsoftware.cron4j.Scheduler;
 
